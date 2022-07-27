85f5633c353b4207d8dcd50107bed94ee627d2ac
Kerrod Young <kerrodyoung@gmail.com>
2022-07-27 23:16:05 +1000

    Create logger for logging to file or console

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//stub
+
+class Logger {
+
+    fs = require('fs')
+
+    console(message) {
+        console.log(`LOGGER: ${message}`)
+    }
+
+    file (message, logfile = 'logs.txt', encoding = 'utf8') {
+        fs.writeFile(logfile, `\n${message}`, { encoding: encoding }, (err) => {
+             `ERROR SAVING TO LOGFILE ${logfile} : console.log(err)`
+        });
+    }
+}
+
+module.exports = Logger
