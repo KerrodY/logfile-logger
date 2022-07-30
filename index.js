3322e031f45b47abb4bd8d9da27da9d8306f4ef4
Kerrod Young <kerrodyoung@gmail.com>
2022-07-30 21:04:08 +1000

    Fix null return value

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     everywhere (message, logfile = 'logs.txt', encode = 'utf8') {
         fs.appendFile(logfile, dateTime.getDateTime() + message + '\n', { encoding: encode }, (err) => {
-            return console(`ERROR SAVING TO LOGFILE ${logfile} : ${err}`)
+            return this.console(`ERROR SAVING TO LOGFILE ${logfile} : ${err}`)
         }); 
         console.log(dateTime.getDateTime() + message + '\n    -> logged @ ' + logfile + '\n')
     }
