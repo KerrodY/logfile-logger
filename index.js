4678023960c481308affb61aaf0e0bcee54bd3ed
Kerrod Young <kerrodyoung@gmail.com>
2022-07-30 21:13:14 +1000

    Fix error handelling

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ module.exports = {
 
     file (message, logfile = 'logs.txt', encode = 'utf8') {
         fs.appendFile(logfile, dateTime.getDateTime() + message + '\n', { encoding: encode }, (err) => {
-            return console(`ERROR SAVING TO LOGFILE ${logfile} : ${err}`)
+            if (err) return this.console(`ERROR SAVING TO LOGFILE ${logfile} : ${err}`)
         });
     },
 
     everywhere (message, logfile = 'logs.txt', encode = 'utf8') {
         fs.appendFile(logfile, dateTime.getDateTime() + message + '\n', { encoding: encode }, (err) => {
-            return this.console(`ERROR SAVING TO LOGFILE ${logfile} : ${err}`)
+            if (err) return this.console(`ERROR SAVING TO LOGFILE ${logfile} : ${err}`)
         }); 
         console.log(dateTime.getDateTime() + message + '\n    -> logged @ ' + logfile + '\n')
     }
