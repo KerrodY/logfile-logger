7dc63b7e183609381cbff3cf0255f4b83bbc4894
Kerrod Young <kerrodyoung@gmail.com>
2022-07-30 20:24:55 +1000

    Add dateTime timestamp to all logs

---
+++ DateTime.js
@@ -0,0 +1,19 @@
+exports.getDateTime = function () {
+
+    var date = new Date();
+    var hour = date.getHours();
+    var min  = date.getMinutes();
+    var sec  = date.getSeconds();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day  = date.getDate();
+
+    hour = (hour < 10 ? "0" : "") + hour;
+    min = (min < 10 ? "0" : "") + min;
+    sec = (sec < 10 ? "0" : "") + sec;
+    month = (month < 10 ? "0" : "") + month;
+    day = (day < 10 ? "0" : "") + day;
+
+    return `[${year}-${month}-${day} | ${hour}:${min}:${sec}] `
+}
+
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const fs = require('fs')
-
+const dateTime = require('DateTime')
 module.exports = {
 
     console(message) {
-        console.log(`LOGGER: ${message}`)
+        console.log(dateTime.getDateTime() + message)
     },
 
-    file (message, logfile = 'logs.txt', encoding = 'utf8') {
-        fs.appendFile(logfile, `${message}\n`, { encoding: encoding }, (err) => {
-             `ERROR SAVING TO LOGFILE ${logfile} : console.log(err)`
+    file (message, logfile = 'logs.txt', encode = 'utf8') {
+        fs.appendFile(logfile, `${dateTime.getDateTime()}${message}\n`, { encoding: encode }, (err) => {
+            return `ERROR SAVING TO LOGFILE ${logfile} : ${console.log(err)}`
         });
     },
 
     everywhere (message, logfile = 'logs.txt', encoding = 'utf8') {
-        fs.appendFile(logfile, `${message}\n`, { encoding: encoding }, (err) => {
-            return `ERROR SAVING TO LOGFILE ${logfile} : console.log(err)`
-        });
-        console.log(`${message} | logged in: ${logfile}`)
+        fs.appendFile(logfile, `${dateTime.getDateTime()}${message}\n`, { encoding: encode }, (err) => {
+            return `ERROR SAVING TO LOGFILE ${logfile} : ${console.log(err)}`
+        }); 
+        console.log(`${message}\n    ↪ logged @ ${logfile}\n`)
     }
 }
 
